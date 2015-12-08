689e3cbc557771b1984d8c8ed2a3fd086b59a8ba
liulinjie <liulj@zjieke.com>
2015-12-08 16:14:57 +0800

    添加百分比布局

---
+++ app/src/main/java/cn/com/llj/demo/activity/other/PercentLayoutDemo.java
@@ -0,0 +1,14 @@
+package cn.com.llj.demo.activity.other;
+
+import cn.com.llj.demo.DemoActivity;
+import cn.com.llj.demo.R;
+
+/**
+ * Created by liulj on 15/12/8.
+ */
+public class PercentLayoutDemo extends DemoActivity {
+    @Override
+    public int getLayoutId() {
+        return R.layout.percent_layout_demo;
+    }
+}
---
+++ app/src/main/java/cn/com/llj/demo/activity/other/RxjavaDemo.java
@@ -0,0 +1,14 @@
+package cn.com.llj.demo.activity.other;
+
+import cn.com.llj.demo.DemoActivity;
+import cn.com.llj.demo.R;
+
+/**
+ * Created by liulj on 15/12/7.
+ */
+public class RxjavaDemo extends DemoActivity {
+    @Override
+    public int getLayoutId() {
+        return R.layout.rxjava_demo;
+    }
+}
